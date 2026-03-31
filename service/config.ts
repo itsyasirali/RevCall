8188161d065fd4d81de9b2bf99a1a3a372bb15b7
Yasir Ali <yasiraliraj@gmail.com>
2026-03-31 17:26:03 +0500

    Solve crash

--- service/config.ts
+++ service/config.ts
@@ -1,7 +1,7 @@
 // export const BASE_URL = 'https://calling-app-backend-production.up.railway.app';
 
 
-export const BASE_URL = 'http://192.168.100.40:5000';
+export const BASE_URL = 'http://192.168.10.17:5000';
 
 
 
